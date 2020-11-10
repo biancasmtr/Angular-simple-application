723ccb409859efb18eca3b5322d286a1a5b71399
Bianca Monteiro <bianca_souza_monteiro@hotmail.com>
2020-11-10 18:23:12 -0300

    commit inicial de aplicação

---
+++ Frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  navigateToProductCreate(): void {
+    alert ("Navegando...")
+  }
+
+}
